77aee4f65cfb951e801037b3c5b4f2932cb49b23
habiba <habiba7141@gmail.com>
2025-09-28 15:27:10 +0300

    add presentation Layer

---
+++ PresentationLayer/Controllers/RegisterationController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Dtos;
+using BusinessLayer.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class RegisterationController : Controller
+    {
+         private readonly IManageRegisteration ManageRegisteration;
+        public RegisterationController(IManageRegisteration manageRegisteration)
+        {
+            ManageRegisteration= manageRegisteration;   
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpGet]
+        public IActionResult AddRegisteration()
+        {
+            return View();
+        }
+
+     
+        [HttpPost]
+        public IActionResult AddRegisteration(RegisterationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ManageRegisteration.addRegisteration(model);
+                return RedirectToAction("Success");
+            }
+            return View(model);
+        }
+
+       
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+
+    }
+
